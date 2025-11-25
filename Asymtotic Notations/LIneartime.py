56da5629dbedb633d116854c63aa99833fe60ba2
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-11-25 06:10:29 +0000

    LInear time complexity

--- Asymtotic Notations/LIneartime.py
+++ Asymtotic Notations/LIneartime.py
@@ -30,10 +30,4 @@ for i in range(n):
     print(i, end=" ")
 
 for i in range(k):
-    print(i, end=" ")
-
-#O(n^2)
-for i in range(n):
-    for j in range(n):
-        print(i, j, end=" ")
-        
+    print(i, end=" ")        
